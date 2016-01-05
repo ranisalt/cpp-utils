5a06e6ee0a283426e13a85dffa46ab9e8123d710
Ranieri Althoff <ranisalt@gmail.com>
2016-01-05 15:45:31 -0200

    Add range (Python-like) class

---
+++ range/range.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include <list>
+#include "range.h"
+
+using range = my::range;
+
+struct range_test: public testing::Test {
+};
+
+TEST_F(range_test, stop) {
+    auto r = range{3};
+
+    auto it = std::begin(r);
+    EXPECT_EQ(0, *it++);
+    EXPECT_EQ(1, *it++);
+    EXPECT_EQ(2, *it++);
+    EXPECT_EQ(std::end(r), it);
+}
+
+TEST_F(range_test, start) {
+    auto r = range{5, 8};
+
+    auto it = std::begin(r);
+    EXPECT_EQ(5, *it++);
+    EXPECT_EQ(6, *it++);
+    EXPECT_EQ(7, *it++);
+    EXPECT_EQ(std::end(r), it);
+}
+
+TEST_F(range_test, step) {
+    auto r = range{10, 18, 3};
+
+    auto it = std::begin(r);
+    EXPECT_EQ(10, *it++);
+    EXPECT_EQ(13, *it++);
+    EXPECT_EQ(16, *it++);
+    EXPECT_EQ(std::end(r), it);
+}
+
+TEST_F(range_test, negativeStep) {
+    auto r = range{0, -5, -2};
+
+    auto it = std::begin(r);
+    EXPECT_EQ(0, *it++);
+    EXPECT_EQ(-2, *it++);
+    EXPECT_EQ(-4, *it++);
+    EXPECT_EQ(std::end(r), it);
+}
+
+TEST_F(range_test, rangeFor) {
+    auto r = range{20, 23};
+    auto expected = std::list<int>{20, 21, 22};
+
+    for (auto i: r) {
+        EXPECT_EQ(expected.front(), i);
+        expected.pop_front();
+    }
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ range/range.h
@@ -0,0 +1,66 @@
+#ifndef RANGE_H
+#define RANGE_H
+
+#include <algorithm>
+#include <iterator>
+
+namespace my {
+
+class range {
+    struct iterator: std::iterator_traits<int> {
+        iterator(int current, const range& owner):
+            current{current}, owner{owner} {}
+
+        iterator& operator++() {
+            auto minmax = std::minmax(current + owner.step, owner.stop);
+            current = owner.step > 0 ? minmax.first : minmax.second;
+            return *this;
+        }
+
+        iterator operator++(int) {
+            auto old = current;
+            this->operator++();
+            return {old, owner};
+        }
+
+        int operator*() const {
+            return current;
+        }
+
+        friend bool operator==(const iterator& lhs, const iterator& rhs) {
+            return lhs.owner == rhs.owner and lhs.current == rhs.current;
+        }
+
+        friend bool operator!=(const iterator& lhs, const iterator& rhs) {
+            return not (lhs.current == rhs.current);
+        }
+
+    private:
+        int current;
+        const range& owner;
+    };
+
+public:
+    range(int stop): start{0}, stop{stop}, step{1} {}
+    range(int start, int stop, int step = 1):
+        start{start}, stop{stop}, step{step} {}
+
+    iterator begin() const {
+        return {start, *this};
+    }
+
+    iterator end() const {
+        return {stop, *this};
+    }
+
+    friend bool operator==(const range& lhs, const range& rhs) {
+        return lhs.start == rhs.start and lhs.stop == rhs.stop and
+            lhs.step == rhs.step;
+    }
+
+    const int start, stop, step;
+};
+
+}
+
+#endif /* RANGE_H */
