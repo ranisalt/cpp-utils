b59491d57d80cb147d091cbc4aafeb3fe2c7c4c5
Ranieri Althoff <ranisalt@gmail.com>
2016-04-22 15:37:48 -0300

    Use bounded lockfree stack, code missing methods

--- lockfree/stack.h
+++ lockfree/stack.h
@@ -1,25 +1,14 @@
 #ifndef LOCKFREE_STACK_H
 #define LOCKFREE_STACK_H
 
+#include <array>
 #include <atomic>
-#include <memory>
 
 namespace hoist {
 namespace lockfree {
 
-template<class T>
+template<class T, std::size_t Capacity>
 class stack {
-    struct node {
-        node(const T& value, node* next):
-                value{value}, next{next} {}
-        node(T&& value, node* next):
-                value{std::move(value)}, next{next} {}
-
-        // for the sake of alignment
-        node* next;
-        T value;
-    };
-
 public:
     using const_reference = const T&;
     using reference = T&;
@@ -27,61 +16,31 @@ public:
     using value_type = T;
 
     stack() = default;
-    explicit stack(const stack<T>& that);
-    explicit stack(stack<T>&& that);
-
-    virtual ~stack() {
-    }
 
     /* accessors */
-    reference top() {
-        auto* p = head.load();
-        return p->value;
-    }
+    reference top() { return buffer[size_ - 1]; }
 
-    const_reference top() const {
-        auto* p = head.load();
-        return p->value;
-    }
+    const_reference top() const { return buffer[size_ - 1]; }
 
     /* capacity */
-    bool empty() const {
-        return size_ == 0;
-    }
+    bool empty() const { return size_ == 0; }
 
-    size_type size() const {
-        return size_;
-    }
+    size_type size() const { return size_; }
 
     /* modifiers */
-    void push(const value_type& value) {
-        auto* p = new node{value, head.load()};
-        while (head.compare_exchange_weak(p->next, p));
-        ++size_;
-    }
+    void push(const value_type& value) { buffer[size_++] = value; }
 
-    void push(value_type&& value) {
-        auto* p = new node{std::move(value), head.load()};
-        while (head.compare_exchange_weak(p->next, p));
-        ++size_;
-    }
+    void push(value_type&& value) { buffer[size_++] = std::move(value); }
 
     template<class ...Args>
     void emplace(Args&& ...args) {
-        auto* p = new node{std::forward<Args>(args)..., head.load()};
-        while (head.compare_exchange_weak(p->next, p));
-        ++size_;
+        buffer[size_++] = {std::forward<Args>(args)...};
     }
 
-    void pop() {
-        auto* p = head.load();
-        while (p && head.compare_exchange_weak(p, p->next));
-        delete p;
-        --size_;
-    }
+    void pop() { std::move(buffer[--size_]); }
 
 private:
-    std::atomic<node*> head{nullptr};
+    std::array<T, Capacity> buffer{};
     std::atomic<size_type> size_{0};
 };
 
--- lockfree/stack_test.cpp
+++ lockfree/stack_test.cpp
@@ -5,18 +5,38 @@ using hoist::lockfree::stack;
 
 struct lockfree_stack_test: public testing::Test
 {
-    stack<int> lfstack;
+    stack<int, 10> lfstack;
 };
 
 TEST_F(lockfree_stack_test, isCreatedEmpty) {
     EXPECT_EQ(0, lfstack.size());
 }
 
-TEST_F(lockfree_stack_test, pushStacks) {
+TEST_F(lockfree_stack_test, pushLvalueStacks) {
+    const int forty_two = 42;
+    const int one_thousand_nine_hundred_sixty_three = 1963;
+    lfstack.push(forty_two);
+    EXPECT_EQ(1, lfstack.size());
+    EXPECT_EQ(forty_two, lfstack.top());
+    lfstack.push(one_thousand_nine_hundred_sixty_three);
+    EXPECT_EQ(one_thousand_nine_hundred_sixty_three, lfstack.top());
+    EXPECT_EQ(2, lfstack.size());
+}
+
+TEST_F(lockfree_stack_test, pushRvalueStacks) {
     lfstack.push(42);
     EXPECT_EQ(1, lfstack.size());
     EXPECT_EQ(42, lfstack.top());
     lfstack.push(1963);
+    EXPECT_EQ(1963, lfstack.top());
+    EXPECT_EQ(2, lfstack.size());
+}
+
+TEST_F(lockfree_stack_test, emplaceStacks) {
+    lfstack.emplace(42);
+    EXPECT_EQ(1, lfstack.size());
+    EXPECT_EQ(42, lfstack.top());
+    lfstack.emplace(1963);
     EXPECT_EQ(1963, lfstack.top());
     EXPECT_EQ(2, lfstack.size());
 }
