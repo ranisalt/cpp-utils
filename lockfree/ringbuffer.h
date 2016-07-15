78313e7ded21a13140ca9304a2d9411dd40955f8
Ranieri Althoff <ranisalt@gmail.com>
2016-07-15 16:16:38 -0300

    Add behavior and tests for value overriding on ringbuffer

--- lockfree/ringbuffer.h
+++ lockfree/ringbuffer.h
@@ -19,52 +19,76 @@ class ringbuffer {
 public:
     using value_type = T;
     using size_type = std::size_t;
-    using reference = value_type &;
+    using reference = value_type&;
     using const_reference = const reference;
 
-    ringbuffer() : rs{new(alloc.aligned_alloc<state>()) state}, re{} {
+    ringbuffer() :
+            rs{new(alloc.aligned_alloc<state>()) state}, re{},
+            flag{LFDS710_MISC_FLAG_LOWERED}, ovr{}
+    {
         lfds710_ringbuffer_init_valid_on_current_logical_core(rs, re, N + 1, nullptr);
     }
 
-    ~ringbuffer() {
-        lfds710_ringbuffer_cleanup(rs, [](state *rs, void *, void *value, lfds710_misc_flag) {
-            delete static_cast<value_type *>(value);
-        });
+    ~ringbuffer()
+    {
+        lfds710_ringbuffer_cleanup(rs,
+                [](state* rs, void*, void* value, lfds710_misc_flag) {
+                    delete static_cast<value_type*>(value);
+                });
     }
 
-    size_type size() const {
+    bool bad() const { return not good(); }
+
+    bool good() const { return flag == LFDS710_MISC_FLAG_LOWERED; }
+
+    size_type size() const
+    {
         lfds710_pal_uint_t size_;
-        lfds710_ringbuffer_query(rs, LFDS710_RINGBUFFER_QUERY_SINGLETHREADED_GET_COUNT, nullptr, &size_);
+        lfds710_ringbuffer_query(rs,
+                LFDS710_RINGBUFFER_QUERY_SINGLETHREADED_GET_COUNT, nullptr,
+                &size_);
         return static_cast<size_type>(size_);
     }
 
-    void push(const value_type &value) {
-        value_type *ptr = new value_type{value};
-        lfds710_ringbuffer_write(rs, nullptr, ptr, nullptr, nullptr, nullptr);
+    void push(const value_type& value)
+    {
+        value_type* ptr = new value_type{value};
+        lfds710_ringbuffer_write(rs, nullptr, ptr, &flag, nullptr, &ovr);
     }
 
-    void push(value_type &&value) {
-        value_type *ptr = new value_type{std::move(value)};
-        lfds710_ringbuffer_write(rs, nullptr, ptr, nullptr, nullptr, nullptr);
+    void push(value_type&& value)
+    {
+        value_type* ptr = new value_type{std::move(value)};
+        lfds710_ringbuffer_write(rs, nullptr, ptr, &flag, nullptr, &ovr);
     }
 
     template<class... Args>
-    void emplace(Args &&... args) {
-        value_type *ptr = new value_type{std::forward<Args>(args)...};
-        lfds710_ringbuffer_write(rs, nullptr, ptr, nullptr, nullptr, nullptr);
+    void emplace(Args&& ... args)
+    {
+        value_type* ptr = new value_type{std::forward<Args>(args)...};
+        lfds710_ringbuffer_write(rs, nullptr, ptr, &flag, nullptr, &ovr);
     }
 
-    value_type pop() {
-        void *re;
+    value_type pop()
+    {
+        void* re;
         lfds710_ringbuffer_read(rs, nullptr, &re);
-        value_type value = *static_cast<value_type *>(re);
+        value_type value = *static_cast<value_type*>(re);
+        return value;
+    }
+
+    value_type overridden()
+    {
+        value_type value = *static_cast<value_type*>(ovr);
         return value;
     }
 
 private:
     detail::allocator<sizeof(state) + detail::atomic_isolation> alloc;
-    state *rs;
+    state* rs;
     lfds710_ringbuffer_element re[N + 1];
+    lfds710_misc_flag flag;
+    void* ovr;
 };
 
 }
--- lockfree/ringbuffer_test.cpp
+++ lockfree/ringbuffer_test.cpp
@@ -1,12 +1,14 @@
 #include <gtest/gtest.h>
 
+#include "../range/range.h"
 #include "ringbuffer.h"
 
 using hoist::lockfree::ringbuffer;
+using hoist::range;
 
 struct lockfree_ringbuffer_test: public testing::Test
 {
-    ringbuffer<int, 4> s;
+    ringbuffer<intmax_t, 4> s;
 };
 
 TEST_F(lockfree_ringbuffer_test, constructor) {
@@ -46,6 +48,27 @@ TEST_F(lockfree_ringbuffer_test, pop) {
     ASSERT_EQ(2, s.size());
     s.pop();
     EXPECT_EQ(1, s.size());
+}
+
+TEST_F(lockfree_ringbuffer_test, bad_good) {
+    for (auto i: range(4)) {
+        s.push(i);
+    }
+    ASSERT_FALSE(s.bad());
+    ASSERT_TRUE(s.good());
+    s.push(42);
+    EXPECT_TRUE(s.bad());
+    EXPECT_FALSE(s.good());
+}
+
+TEST_F(lockfree_ringbuffer_test, overridden) {
+    for (auto i: range(4)) {
+        s.push(i);
+    }
+    s.push(42);
+    EXPECT_EQ(0, s.overridden());
+    s.push(1963);
+    EXPECT_EQ(1, s.overridden());
 }
 
 int main(int argc, char** argv) {
